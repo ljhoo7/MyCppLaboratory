090a6261e7da0071f13107b18e2b759c01cbd9f4
jeonghunlee <ljhoo8@naver.com>
2021-04-29 23:39:30 +0900

    length & typeat of typelist compile time string practice

--- CompileTimeStringParsingTest/Case2-TypelistWithVariadicTemplate.cpp
+++ CompileTimeStringParsingTest/Case2-TypelistWithVariadicTemplate.cpp
@@ -4,5 +4,21 @@
 
 void case2()
 {
-	typelist<int, short, long, float> tl;
+	typedef typelist<int, short, long, float> tl;
+
+	// Test 1 - Length of typelist
+	{
+		static_assert(4 == length<tl::type>::value);
+
+		//// compile-error
+		// static_assert(3 == length<tl::type>::value);
+	}
+
+	// Test 2 - TypeAt of typelist
+	{
+		static_assert(true == std::is_same_v<short, type_at<tl::type, 1>::type>);
+
+		//// compile-error
+		// static_assert(true == std::is_same_v<int, type_at<tl::type, 1>::type>);
+	}
 }
--- CompileTimeStringParsingTest/Case2-TypelistWithVariadicTemplate.h
+++ CompileTimeStringParsingTest/Case2-TypelistWithVariadicTemplate.h
@@ -2,6 +2,12 @@
 
 struct null_type {};
 
+/// <summary>
+/// typelist
+/// </summary>
+/// <typeparam name="Head"></typeparam>
+/// <typeparam name="Tail"></typeparam>
+
 template <typename Head, typename Tail>
 struct typenode
 {
@@ -22,6 +28,61 @@ template <typename Head, typename... Rest>
 struct typelist<Head, Rest...>
 {
 	typedef typenode<Head, typename typelist<Rest...>::type> type;
+};
+
+/// <summary>
+/// Length of typelist
+/// </summary>
+/// <typeparam name="TL"></typeparam>
+
+template <typename TL>
+struct length
+{
+	static const int value = 1 + length<typename TL::tail_type>::value;
+};
+
+template<>
+struct length<null_type>
+{
+	static const int value = 0;
+};
+
+/// <summary>
+/// TypeAt of typelist
+/// </summary>
+/// <typeparam name="TL"></typeparam>
+
+template <typename TL, int index>
+struct type_at
+{
+	typedef typename type_at<typename TL::tail_type, index - 1>::type type;
+};
+
+template <typename Head, typename Tail>
+struct type_at<typenode<Head, Tail>, 0>
+{
+	typedef Head type;
+};
+
+/// <summary>
+/// visitor of typelist
+/// </summary>
+/// <typeparam name="TL"></typeparam>
+
+template <typename TL, template <class> class Delegator>
+struct visitor
+{
+	static void execute()
+	{
+		Delegator<typename TL::head_type>::execute();
+		visitor<typename TL::tail_type, Delegator>::execute();
+	}
+};
+
+template <template <class> class Delegator>
+struct visitor<null_type, Delegator>
+{
+	static void execute() {}
 };
 
 void case2();
