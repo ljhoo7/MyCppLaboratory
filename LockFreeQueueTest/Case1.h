91c6f4f03c815521d450a9b3ec86dcec9b57a212
jeonghunlee <ljhoo8@naver.com>
2021-08-14 19:24:52 +0900

    Build error fixed

--- LockFreeQueueTest/Case1.h
+++ LockFreeQueueTest/Case1.h
@@ -35,10 +35,81 @@ private:
     Node* _tail;        // 마지막노드를 포인트한다.
 
 public:
-    QueueT();
+	QueueT()
+	{
+		_size = 0;
+		_head = new Node;
+		_head->next = NULL;
+		_tail = _head;
+	}
 
     void Enqueue(T t);
     int Dequeue(T& t);
 };
 
-#include "Case1.cpp"
+
+template<typename T>
+void QueueT<T>::Enqueue(T t)
+{
+	Node* node = new Node;
+	node->data = t;
+	node->next = NULL;
+
+	while (true)
+	{
+		Node* tail = _tail;
+		Node* next = tail->next;
+
+		if (tail == _tail)
+		{
+			if (next == NULL)
+			{
+				if (InterlockedCompareExchangePointer((PVOID*)&tail->next, node, next) == next)
+				{
+					InterlockedCompareExchangePointer((PVOID*)&_tail, node, tail);
+					break;
+				}
+			}
+			else
+			{
+				InterlockedCompareExchangePointer((PVOID*)&_tail, next, tail);
+			}
+
+		}
+	}
+
+	InterlockedExchangeAdd(&_size, 1);
+}
+
+template<typename T>
+int QueueT<T>::Dequeue(T& t)
+{
+	if (_size == 0)
+		return -1;
+
+	while (true)
+	{
+		Node* head = _head;
+		Node* tail = _tail;
+		Node* next = head->next;
+		if (head == _head)
+		{
+			if (head == tail)
+			{
+				if (next == NULL)
+					return -1;
+			}
+			else
+			{
+				t = next->data;
+				if (InterlockedCompareExchangePointer((PVOID*)&_head, next, head) == head)
+				{
+					delete head;
+					break;
+				}
+			}
+		}
+	}
+	InterlockedExchangeAdd(&_size, -1);
+	return 0;
+}
