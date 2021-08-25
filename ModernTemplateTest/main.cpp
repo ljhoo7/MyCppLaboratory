e4cdd9e689c8b201ab4ef699b045c71c5428b61c
jeonghunlee <ljhoo8@naver.com>
2021-08-25 23:04:34 +0900

    C++ template guide 2nd example

--- ModernTemplateTest/main.cpp
+++ ModernTemplateTest/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+struct Node {
+	int value;
+
+	Node* left {nullptr};
+	Node* right {nullptr};
+
+	Node(int i = 0) : value(i) {}
+};
+
+auto left = &Node::left;
+auto right = &Node::right;
+
+template<typename T, typename... TP>
+Node* traverse(T np, TP... paths)
+{
+	return (np ->* ...->*paths);
+}
+
 int main()
 {
+	Node* root = new Node();
+	
+	root->left = new Node { 1 };
+	root->right = new Node{ 3 };
+	root->left->right = new Node{ 2 };
+	root->left->right->left = new Node{ 4 };
+
+	Node* node = traverse(root, left, right, left);
+
+	std::cout << node->value << std::endl;
 }
