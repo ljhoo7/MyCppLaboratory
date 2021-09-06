875ca4dcb6139c7757053aaef8957086fe8ca140
jeonghunlee <ljhoo8@naver.com>
2021-09-06 22:03:57 +0900

    non type variadic template function practice

--- CompileTimeStringParsingTest/Case3.cpp
+++ CompileTimeStringParsingTest/Case3.cpp
@@ -2,8 +2,16 @@
 
 void Case3()
 {
-	Test<'T','E','S','T'>();
+	/*Test<'T','E','S','T'>();
 	Test<'T', 'E', 'S'>();
 	Test<'T', 'E'>();
-	Test<'T'>();
+	Test<'T'>();*/
+
+	static const char test[] = "Test1";
+	Test<test>();
+	Test<test>();
+
+	static const char test2[] = "Test2";
+	Test<test2>();
+	Test<test2>();
 }
--- CompileTimeStringParsingTest/Case3.h
+++ CompileTimeStringParsingTest/Case3.h
@@ -4,8 +4,26 @@
 
 void Case3();
 
-template<char CHAR1, char CHAR2 = ' ', char CHAR3 = ' ', char CHAR4 = ' '>
+template<char CHAR1, char... REST>
 void Test()
 {
-	std::cout << CHAR1 << CHAR2 << CHAR3 << CHAR4 << std::endl;
+    std::cout << CHAR1;
+
+    if constexpr (0 < sizeof...(REST))
+    {
+        Test<REST...>();
+    }
+    else
+    {
+        std::cout << std::endl;
+    }
+}
+
+template<const char* STRING>
+void Test()
+{
+    static int tmp = 0;
+
+    std::cout << tmp++ << ' ';
+    std::cout << STRING << std::endl;
 }
