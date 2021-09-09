190483e51eeb76f2792cec98ac79120f93fc1d26
jeonghunlee <ljhoo8@naver.com>
2021-09-10 01:51:43 +0900

    use __LINE__ as variable name

--- CompileTimeStringParsingTest/Case3.cpp
+++ CompileTimeStringParsingTest/Case3.cpp
@@ -1,10 +1,16 @@
 #include "Case3.h"
 
-#define Method(NAME, PATH,CALLABLE) static const char NAME[]{ PATH };Test<NAME>(CALLABLE)
+#define COMBINE_INTERNAL(X,Y) X##Y
+#define COMBINE(X,Y) COMBINE_INTERNAL(X,Y)
+#define PUT(PATH,CALLABLE) static const char COMBINE(PUT,__LINE__)[]{ PATH };Test<COMBINE(PUT,__LINE__)>(CALLABLE)
 
 void Case3()
 {
-	Method(a, "test/test2", []() {
+	PUT("test/test2", []() {
 		std::cout<<"hello"<<std::endl;
 	});
+
+	PUT("test/test2", []() {
+		std::cout << "hello" << std::endl;
+		});
 }
