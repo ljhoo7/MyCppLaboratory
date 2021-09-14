906910c36dc87e3ad483322a1f961db4c2d1671d
jeonghunlee <ljhoo8@naver.com>
2021-09-14 22:28:52 +0900

    clean

--- CompileTimeStringParsingTest/Case3.cpp
+++ CompileTimeStringParsingTest/Case3.cpp
@@ -1,9 +1,5 @@
 #include "Case3.h"
 
-#define COMBINE_INTERNAL(X,Y) X##Y
-#define COMBINE(X,Y) COMBINE_INTERNAL(X,Y)
-#define PUT(PATH,CALLABLE) static const char COMBINE(PUT,__LINE__)[]{ PATH };Test<COMBINE(PUT,__LINE__)>(CALLABLE)
-
 void Case3()
 {
 	PUT("test/test2", []() {
--- CompileTimeStringParsingTest/Case3.h
+++ CompileTimeStringParsingTest/Case3.h
@@ -10,3 +10,7 @@ void Test(CALLABLE callable)
 {
     callable();
 }
+
+#define COMBINE_INTERNAL(X,Y) X##Y
+#define COMBINE(X,Y) COMBINE_INTERNAL(X,Y)
+#define PUT(PATH,CALLABLE) static const char COMBINE(PUT,__LINE__)[]{ PATH };Put<COMBINE(PUT,__LINE__)>(CALLABLE)
