fc19e196ff7d4f17b0c9a7dfe3c21191b943d395
jeonghunlee <ljhoo8@naver.com>
2021-09-15 22:32:49 +0900

    Making callable static router class

--- CompileTimeStringParsingTest/Case3.cpp
+++ CompileTimeStringParsingTest/Case3.cpp
@@ -2,11 +2,21 @@
 
 void Case3()
 {
-	PUT("test/test2", []() {
-		std::cout<<"hello"<<std::endl;
-	});
-
-	PUT("test/test2", []() {
+	/*PUT("test/test2", []() {
 		std::cout << "hello" << std::endl;
+		});*/
+
+	static const char path[] = "test/test2";
+
+	Put<path> tmp;
+
+	tmp.Enroll([]()
+		{
+			std::cout << "test" << std::endl;
 		});
+
+	Put<path>::g_callable();
 }
+
+template<const char* PATH>
+std::function<void()> Put<PATH>::g_callable;
--- CompileTimeStringParsingTest/Case3.h
+++ CompileTimeStringParsingTest/Case3.h
@@ -14,3 +14,26 @@ void Test(CALLABLE callable)
 #define COMBINE_INTERNAL(X,Y) X##Y
 #define COMBINE(X,Y) COMBINE_INTERNAL(X,Y)
 #define PUT(PATH,CALLABLE) static const char COMBINE(PUT,__LINE__)[]{ PATH };Put<COMBINE(PUT,__LINE__)>(CALLABLE)
+
+template<const char* PATH>
+class Put
+{
+public:
+	static std::function<void()> g_callable;
+
+	//void 
+
+	template<typename CALLABLE>
+	void Enroll(CALLABLE callable)
+	{
+		if (nullptr != g_callable)
+		{
+
+			return;
+		}
+
+		g_callable = callable;
+	}
+
+
+};
