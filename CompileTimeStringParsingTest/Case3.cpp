0a5331558b6cc356d14c606dcef7d003f4359e8c
jeonghunlee <ljhoo8@naver.com>
2021-09-16 23:50:08 +0900

    Making static router class completed.

--- CompileTimeStringParsingTest/Case3.cpp
+++ CompileTimeStringParsingTest/Case3.cpp
@@ -8,15 +8,13 @@ void Case3()
 
 	static const char path[] = "test/test2";
 
-	Put<path> tmp;
-
-	tmp.Enroll([]()
+	PUT<path>::Register([]()
 		{
 			std::cout << "test" << std::endl;
 		});
 
-	Put<path>::g_callable();
+	PUT<path>::g_callable();
 }
 
 template<const char* PATH>
-std::function<void()> Put<PATH>::g_callable;
+std::function<void()> PUT<PATH>::g_callable;
--- CompileTimeStringParsingTest/Case3.h
+++ CompileTimeStringParsingTest/Case3.h
@@ -5,35 +5,25 @@
 
 void Case3();
 
-template<const char* PATH, typename CALLABLE>
-void Test(CALLABLE callable)
-{
-    callable();
-}
-
-#define COMBINE_INTERNAL(X,Y) X##Y
-#define COMBINE(X,Y) COMBINE_INTERNAL(X,Y)
-#define PUT(PATH,CALLABLE) static const char COMBINE(PUT,__LINE__)[]{ PATH };Put<COMBINE(PUT,__LINE__)>(CALLABLE)
+//#define COMBINE_INTERNAL(X,Y) X##Y
+//#define COMBINE(X,Y) COMBINE_INTERNAL(X,Y)
+//#define PUT(PATH,CALLABLE) static const char COMBINE(PUT,__LINE__)[]{ PATH };Put<COMBINE(PUT,__LINE__)>(CALLABLE)
 
 template<const char* PATH>
-class Put
+class PUT
 {
 public:
 	static std::function<void()> g_callable;
 
-	//void 
-
 	template<typename CALLABLE>
-	void Enroll(CALLABLE callable)
+	static void Register(CALLABLE callable)
 	{
 		if (nullptr != g_callable)
 		{
-
+			throw new std::exception("test");
 			return;
 		}
 
 		g_callable = callable;
 	}
-
-
 };
